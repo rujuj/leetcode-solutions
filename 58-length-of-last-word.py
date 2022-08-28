a0e2bd40ae9fcd1eb64bca7da7a65ab09dea4d2d
Ruju <rujykr@gmail.com>
2022-08-28 01:32:34 -0400

    realized i didn't need to store anything anymore

--- 58-length-of-last-word.py
+++ 58-length-of-last-word.py
@@ -2,19 +2,15 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         
         # replace words = s.split(" ") with:
-        words = []
+        
         oldChar = ""
         currWord = ""
         
         for char in s:
             if char != " ":
                 if oldChar == " ": # this signifies the start of a new word
-                    words.append(currWord) # add the old word to the list of words
+                    # words.append(currWord) # we don't even need this here .. just get the last word
                     currWord = "" # reset!
                 currWord += char # add the new alphabet character into the current word being tracked
             oldChar = char
-        words.append(currWord)
-                    
-        while "" in words:
-            words.remove("")
-        return len(words[-1])
+        return len(currWord)
