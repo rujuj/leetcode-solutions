cb318ee5aed43457e4ea3552deb4dc1d708507c1
Ruju <rujykr@gmail.com>
2022-08-28 02:30:22 -0400

    started from back instead

--- 58-length-of-last-word.py
+++ 58-length-of-last-word.py
@@ -1,21 +1,16 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        
-        # replace words = s.split(" ") with:
-        
+        count = 0
         oldChar = ""
-        currCount = 0
-        
-        for char in s:
-            if char != " ":
+        for i in range(len(s) - 1, -1, -1):
+            if s[i]!= " ":
                 if oldChar == " ":  # this signifies the start of a new word
-                    # words.append(currWord) # we don't even need this here .. just get the last word
-                    currCount = 0  # reset!
-                currCount += 1  # add the new alphabet character into the current word being tracked
-            oldChar = char
-        return currCount
-    
+                    if count > 0:
+                        return count
+                    count = 0  # reset!
+                count += 1  # add the new alphabet character into the current word being tracked
+            oldChar = s[i]
+        return count
     
     # other ways to make this more efficient:
-    # just start at the back (who says we need to go through the whole string?)
     # there must be a better way to keep track of the previous character?
