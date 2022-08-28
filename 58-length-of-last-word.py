38eb8f259ed42ea263a12ada75d43ac6a14576ea
Ruju <rujykr@gmail.com>
2022-08-28 01:26:56 -0400

    rewrote split to skip over the spaces so it's more efficient

--- 58-length-of-last-word.py
+++ 58-length-of-last-word.py
@@ -1,6 +1,20 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        words = s.split(" ")
+        
+        # replace words = s.split(" ") with:
+        words = []
+        oldChar = ""
+        currWord = ""
+        
+        for char in s:
+            if char != " ":
+                if oldChar == " ": # this signifies the start of a new word
+                    words.append(currWord) # add the old word to the list of words
+                    currWord = "" # reset!
+                currWord += char # add the new alphabet character into the current word being tracked
+            oldChar = char
+        words.append(currWord)
+                    
         while "" in words:
             words.remove("")
         return len(words[-1])
