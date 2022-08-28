169c271abe2674c06220ddbab6e80a5bfd832e85
Ruju <rujykr@gmail.com>
2022-08-28 01:47:49 -0400

    just counted the length instead of keeping track of the word

--- 58-length-of-last-word.py
+++ 58-length-of-last-word.py
@@ -4,19 +4,18 @@ class Solution:
         # replace words = s.split(" ") with:
         
         oldChar = ""
-        currWord = ""
+        currCount = 0
         
         for char in s:
             if char != " ":
-                if oldChar == " ": # this signifies the start of a new word
+                if oldChar == " ":  # this signifies the start of a new word
                     # words.append(currWord) # we don't even need this here .. just get the last word
-                    currWord = "" # reset!
-                currWord += char # add the new alphabet character into the current word being tracked
+                    currCount = 0  # reset!
+                currCount += 1  # add the new alphabet character into the current word being tracked
             oldChar = char
-        return len(currWord)
+        return currCount
     
     
     # other ways to make this more efficient:
     # just start at the back (who says we need to go through the whole string?)
     # there must be a better way to keep track of the previous character?
-    
